bf1757376815d35c6733702f6599a17b8cab6264
Jessica A Udo <jessica.udo@mail.mcgill.ca>
2020-10-02 12:24:51 -0700

    Started my technical portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Jessica Adaobi Udo (she/her)', // Title for your website.
+    tagline: 'TECHONOLOGY AND IMPACT: Program Manager, Microsoft Data Insights. Impact Entrepreneurship at African Impact Initiative',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://jessicaudo.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'jessicaudo',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'jessicaudo',
+        twitter: 'jess_udo',
+        github: 'jessicaudo',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
